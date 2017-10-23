9ac51a9ff1ea38b24f81f0db292dcdb072804d96
rag2310 <rag.ortega@gmail.com>
2017-10-23 16:24:35 -0600

    se agrego la configuracion del FIREBASE

--- app/router.js
+++ app/router.js
@@ -3,8 +3,11 @@ import homepage from './homepage'
 import contacto from './contactos'
 import acercade from './acercade'
 import busqueda from './busqueda'
-import biblioteca from './biblioteca'
+//import biblioteca from './biblioteca'
 import layout from './layout'
+import firebase from 'firebase'
+import config from './config'
+
 
 
 page('/', () => {
@@ -28,6 +31,39 @@ page('/busqueda', () => {
 })
 
 page('/biblioteca', () => {
-	const main = document.querySelector('main')
-	main.innerHTML = layout(biblioteca)
+	var main = document.querySelector('main')
+	if (!firebase.apps.length) {
+	firebase.initializeApp(config)
+	}
+
+	var db = firebase.database()
+
+	function obtenerDatos (datos) {
+		var arrayDatos = datos.val()
+		var main = document.querySelector('main')
+		var arrayHTML = ''
+		for (var i = 0; i < arrayDatos.length; i++) {
+			arrayHTML += `
+				<li>
+					${arrayDatos[i].nombre}
+				<li>
+			`
+		}
+
+		var html = `
+			<h1>Lista de juegos</h1>
+			<ul>
+				${arrayHTML}
+			</ul>
+		`
+
+		main.innerHTML = layout(html)
+	}
+	db.ref('games').once('value').then(obtenerDatos)
+	//main.innerHTML = layout(biblioteca)
 })
+
+//page('/biblioteca', () => {
+//	const main = document.querySelector('main')
+//	main.innerHTML = layout(biblioteca)
+//})
---
+++ config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBunskatXxfnyok9TLeLJ1eBnkDtYmqI7s",
+    authDomain: "dbgamestore-2ad64.firebaseapp.com",
+    databaseURL: "https://dbgamestore-2ad64.firebaseio.com",
+    projectId: "dbgamestore-2ad64",
+    storageBucket: "dbgamestore-2ad64.appspot.com",
+    messagingSenderId: "192117113206"
+  }
+
+export default config
