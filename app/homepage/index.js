9bf67220592d2953a7622fdb6ed8332f3c8c0d07
unknown <1310615@H4023.ei.ing.edu.ni>
2017-10-24 00:14:17 +0200

    Estilo de homepage

--- app/homepage/index.js
+++ app/homepage/index.js
@@ -1,3 +1,129 @@
-const index = `<h1>HOMEPAGE</h1>`
+const index = `
+<div id="site-content">
+			<div class="home-slider">
+				<ul class="slides">
+					<li data-bg-image="">
+						<div class="container">
+						<img src="https://d1z4o56rleaq4j.cloudfront.net/images/assets/Play-Killzone/_heroM/686/Killzone-Shadow-Fall.jpg?mtime=20160414075949" class="slide-image">
+							<div class="slide-content">
+								<h2 class="slide-title">Kill Zone 3</h2>
+								<small class="slide-subtitle"></small>
+								
+								<p>Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</p>
+								
+								<a href="/" class="button">boton</a>
+							</div>
+
+							
+						</div>
+					</li>
+					
+				</ul> <!-- .slides -->
+			</div> <!-- .home-slider -->
+
+			<main class="main-content">
+				<div class="container">
+					<div class="page">
+						<section>
+							<header>
+								<h2 class="section-title">Nuevos productos</h2>
+								<a href="/" class="all">Ver todos</a>
+							</header>
+
+							<div class="product-list">
+								<div class="product">
+									<div class="inner-product">
+										<div class="figure-image">
+											<a href="single.html"><img src="" alt="game 1"></a>
+										</div>
+										<h3 class="product-title"><a href="/">Nombre </a></h3>
+										<small class="price"></small>
+										<p>Lorem ipsum dolor sit consectetur adipiscing elit do eiusmod tempor...</p>
+									
+										
+										
+									</div>
+								</div> <!-- .product -->
+
+								<div class="product">
+									<div class="inner-product">
+										<div class="figure-image">
+											<a href="single.html"><img src="" alt="Game 2"></a>
+										</div>
+										<h3 class="product-title"><a href="/">Nombre</a></h3>
+										<small class="price"></small>
+										<p>Lorem ipsum dolor sit consectetur adipiscing elit do eiusmod tempor...</p>
+									</div>
+								</div> <!-- .product -->
+
+								<div class="product">
+									<div class="inner-product">
+										<div class="figure-image">
+											<a href="single.html"><img src="" alt="Game 3"></a>
+										</div>
+										<h3 class="product-title"><a href="/">Nombre</a></h3>
+										<small class="price"></small>
+										<p>Lorem ipsum dolor sit consectetur adipiscing elit do eiusmod tempor...</p>
+									
+									</div>
+								</div> <!-- .product -->
+
+								
+
+
+
+							</div> <!-- .product-lista -->
+
+						</section>
+
+						<section>
+							
+
+						</section>
+					</div>
+				</div> <!-- .container -->
+			</main> <!-- .main-content -->
+
+			<div class="site-footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-2">
+							<div class="widget">
+								<h3 class="widget-title">Informacion</h3>
+								<ul class="no-bullet">
+									<li><a href="/contactos">contactanos</a></li>
+									<li><a href="/acercade">Acerca de </a></li>
+									<li><a href="/biblioteca">Biblioteca</a></li>
+									<li><a href="/busqueda">Busqueda</a></li>
+								</ul>
+							</div> <!-- .widget -->
+						</div> <!-- column -->
+					
+					
+
+		<div class="overlay"></div>
+
+		<div class="auth-popup popup">
+			<a href="#" class="close"><i class="fa fa-times"></i></a>
+			<div class="row">
+				<div class="col-md-6">
+					<h2 class="section-title">Login</h2>
+					<form action="#">
+						<input type="text" placeholder="Username...">
+						<input type="password" placeholder="Password...">
+						<input type="submit" value="Login">
+					</form>
+				</div> <!-- .column -->
+				<div class="col-md-6">
+					<h2 class="section-title">Create an account</h2>
+					<form action="#">
+						<input type="text" placeholder="Username...">
+						<input type="text" placeholder="Email address...">
+						<input type="submit" value="register">
+					</form>
+				</div> <!-- .column -->
+			</div> <!-- .row -->
+		</div> <!-- .auth-popup -->
+`
 
 export default index
--- app/menu/index.js
+++ app/menu/index.js
@@ -1,23 +1,31 @@
 const menu = `
-	<nav>
-		<ul>
-			<li>
-				<a href="/">Inicio</a>
-			</li>
-			<li>
-				<a href="/contacto">Contacto</a>
-			</li>
-			<li>
-				<a href="/acercade">Acerca de</a>
-			</li>
-			<li>
-				<a href="/busqueda">busqueda</a>
-			</li>
-			<li>
-				<a href="/biblioteca">biblioteca</a>
-			</li>
-		</ul>
-	</nav>
+		<div class="site-header">
+				<div class="container">
+					<a href="index.html" id="branding">
+						<img src="" alt="" class="logo">
+						<div class="logo-text">
+							<h1 class="site-title">BIZZARRO STORE</h1>
+							<small class="site-description">Todo en juegos</small>
+						</div>
+					</a> <!-- #branding -->
+
+					<div class="main-navigation">
+						<button class="toggle-menu"><i class="fa fa-bars"></i></button>
+						<ul class="menu">
+							<li class="menu-item home current-menu-item"><a href="/"><i class="icon-home"></i></a></li>
+							<li class="menu-item"><a href="/">Inicio</a></li>
+							<li class="menu-item"><a href="/contactos">Contactanos</a></li>
+							<li class="menu-item"><a href="/acercade">acerca de</a></li>
+							<li class="menu-item"><a href="/biblioteca">Biblioteca</a></li>
+							<li class="menu-item"><a href="/busqueda">Busqueda</a></li>
+						
+						</ul> <!-- .menu -->
+						
+
+						<div class="mobile-navigation"></div> <!-- .mobile-navigation -->
+					</div> <!-- .main-navigation -->
+				</div> <!-- .container -->
+			</div> <!-- .site-header -->
 `
 
 export default menu
