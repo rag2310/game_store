f6da7ba9934c767f6fe91d0efbfd493e5722e877
xlop29 <xlopez2992@gmail.com>
2017-10-29 21:01:24 -0600

    creacion del diseño de la pagina detalles

--- app/detalle/index.js
+++ app/detalle/index.js
@@ -14,9 +14,34 @@ page('/detalle/:codigoGame', (ctx, next) => {
 	db.ref('/games/' + ctx.params.codigoGame).once('value').then((snapshot) => {
 		let game = snapshot.val()
 		let html = `
-			<div>
-				<h1>${game.nombre}</h1>
-			</div>
+
+
+
+
+				<div class="container">
+					<div class="page">
+
+						<div class="entry-content">
+							<div class="row">
+								<div class="col-sm-6 col-md-4">
+									<div class="product-images">
+										<figure class="large-image">
+											<a href=""><img src="${game.url}"></a>
+										</figure>
+									</div>
+								</div>
+								<div class="col-sm-6 col-md-8">
+									<h2 class="entry-title">${game.nombre}</h2>
+									<small class="price"> $ ${game.precio}</small>
+
+									<p>${game.descripcion}</p>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div> <!-- .container -->
 		`
 
 		const main = document.querySelector('main')
