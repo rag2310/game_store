7a72388005691fef71d9921fc4aef9cb338cfa05
xlop29 <xlopez2992@gmail.com>
2017-11-07 09:47:31 -0600

    haciendo commit

--- app/nuevojuego/index.js
+++ app/nuevojuego/index.js
@@ -7,57 +7,52 @@ import config from './../config'
 var db = firebase.database()
 
 			debugger;
-			const index = `
-			<div class="page">
-						<table class="cart">
-							<tbody>
-								<tr>
-									<td class="product-name">
-									<div class="row">
-			<div class="col-md-6" style ="text-align:center">
-				<h2 class="section-title">Nombre</h2>
-				<form action="#">
-					<input type="text" placeholder="Nombre">
-					<input type="submit" value="boton">
-				</form>
-			</div> <!-- .column -->
-			<div class="col-md-6" style ="text-align:center">
-				<h2 class="section-title">Nombre</h2>
-				<form action="#">
-					<input type="text" placeholder="Nombre">
-					<input type="submit" value="boton">
-				</form>
-
-			</div> <!-- .column -->
-
-
-			<div class="col-md-6" style ="text-align:center">
-				<h2 class="section-title" >Nombre</h2>
-				<form action="#">
-					<input type="text" placeholder="Nombre">
-					<input type="submit" value="boton">
-				</form>
-			</div> <!-- .column -->
-
-			<div class="col-md-6" style ="text-align:center">
-				<h2 class="section-title">Nombret</h2>
-				<form action="#">
-					<input type="text" placeholder="Nombre">
-					<input type="submit" value="boton">
-				</form>
-
-				<div class="col-md-6" >
-					<form action="#" style ="text-align:20px">
-						<input type="submit" value="boton" >
-					</form>
-			</div> <!-- .column -->
-
-		</div> <!-- .row -->
-
-									</td>
-						</table>
-
-
-			`
+const index = `
+<div class="page">
+	<table class="cart">
+		<tbody>
+			<tr>
+				<td class="product-name">
+					<div class="row">
+						<div class="col-md-6" style ="text-align:center">
+							<h2 class="section-title">Nombre</h2>
+							<form action="#">
+								<input type="text" placeholder="Nombre">
+								<input type="submit" value="boton">
+							</form>
+						</div> <!-- .column -->
+						<div class="col-md-6" style ="text-align:center">
+							<h2 class="section-title">Nombre</h2>
+							<form action="#">
+								<input type="text" placeholder="Nombre">
+								<input type="submit" value="boton">
+							</form>
+						</div> <!-- .column -->
+						<div class="col-md-6" style ="text-align:center">
+							<h2 class="section-title" >Nombre</h2>
+							<form action="#">
+								<input type="text" placeholder="Nombre">
+								<input type="submit" value="boton">
+							</form>
+						</div> <!-- .column -->
+						<div class="col-md-6" style ="text-align:center">
+							<h2 class="section-title">Nombret</h2>
+							<form action="#">
+								<input type="text" placeholder="Nombre">
+								<input type="submit" value="boton">
+							</form>
+							<div class="col-md-6" >
+							<form action="#" style ="text-align:20px">
+								<input type="submit" value="boton" >
+							</form>
+							</div> 
+						</div> <!-- .column -->
+					</div>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+`
 
 export default index
