9d414508fea7b0496b3675fa3a869b2834d78594
Rodolfo Gutierrez <rag.ortega@gmail.com>
2017-11-11 15:15:24 +0100

    se reparon un bug el cual evitaba que se cargara la pestaña de tienda causando que al guardar no se cambiaba de pestaña

--- app/tienda/index.js
+++ app/tienda/index.js
@@ -10,55 +10,55 @@ var db = firebase.database()
 
 
 const cargarDatosTienda = () => {
+	function obtenerDatos (dato) {
+		var html = ''
+		var htmlGame = ''
+		var index = ''
+		
+			const datos = dato.val()
 
-function obtenerDatos (dato) {
-	var html = ''
-	var htmlGame = ''
-	var index = ''
-	
-		const datos = dato.val()
+			const keys = Object.keys(datos)
 
-		const keys = Object.keys(datos)
+			for( var i = 0; i <keys.length; i++) {
+				const key = keys[i]
+				const game = datos[key]
 
-		for( var i = 0; i <keys.length; i++) {
-			const key = keys[i]
-			const game = datos[key]
-
-			htmlGame = `
-				<div class="product">
-					<div class="inner-product">
-						<div class="figure-image">
-							<img src="${game.url}">
+				htmlGame = `
+					<div class="product">
+						<div class="inner-product">
+							<div class="figure-image">
+								<img src="${game.url}">
+							</div>
+							<h3 class="product-title"><a href="/detalle/${key}">${game.nombre}</a></h3>
+							<small class="price">Precio: $ ${game.precio}</small>
 						</div>
-						<h3 class="product-title"><a href="/detalle/${key}">${game.nombre}</a></h3>
-						<small class="price">Precio: $ ${game.precio}</small>
+
 					</div>
+				`
+				html += htmlGame
+			}
 
+			index = `
+			<main class="main-content">
+				<div class="container">
+					<div class="page">
+						<section>
+							<header>
+								<h2 class="section-title">Tienda</h2>
+							</header>
+							<div class="product-list">
+								${html}
+							</div>
+						</section>
+					</div>
 				</div>
+			</main>
 			`
-			html += htmlGame
-		}
+			const main = document.querySelector('main')
+			main.innerHTML = index
 
-		index = `
-		<main class="main-content">
-			<div class="container">
-				<div class="page">
-					<section>
-						<header>
-							<h2 class="section-title">Tienda</h2>
-						</header>
-						<div class="product-list">
-							${html}
-						</div>
-					</section>
-				</div>
-			</div>
-		</main>
-		`
-		const main = document.querySelector('main')
-		main.innerHTML = index
-
+	
+}
 db.ref('games').once('value').then(obtenerDatos)
 }
-
 export default cargarDatosTienda
